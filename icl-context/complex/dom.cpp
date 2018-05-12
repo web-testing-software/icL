3ee1c53532b1fa950f4fdaa3009e3ac63547be7c
Vasile Lelițac <lixcode@vivaldi.net>
2018-05-12 09:55:44 +0300

    Dom order methods are ready.

--- icl-context/complex/dom.cpp
+++ icl-context/complex/dom.cpp
@@ -6,4 +6,19 @@ Dom::Dom () {
 
 }
 
+bool Dom::checkPrev (const Context *context) const {
+	return
+		context == nullptr               ||
+		context->role () == Role::Assign ||
+		( context->role () != Role::Exists && context->isResultative () );
+}
+
+bool Dom::canBeAtEnd () const {
+	return false;
+}
+
+Context * Dom::runMethod (const QString &name, memory::ParamList &params) {
+
+}
+
 } // namespace
--- icl-context/complex/dom.h
+++ icl-context/complex/dom.h
@@ -1,13 +1,22 @@
 #ifndef DOM_H
 #define DOM_H
 
+#include "complex.h"
+
+
 
 namespace vm::context::complex {
 
-class Dom
+class Dom : Complex
 {
 public:
 	Dom ();
+
+	// Context interface
+public:
+	bool checkPrev(const Context *context) const override;
+	bool canBeAtEnd() const override;
+	Context *runMethod(const QString &name, memory::ParamList &params) override;
 };
 
 } // namespace
