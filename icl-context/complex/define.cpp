f29e69723e1c65d9b6609977a315f67c03b33094
Vasile Lelițac <lixcode@vivaldi.net>
2018-05-12 09:59:07 +0300

    Define base methos realeased.

--- icl-context/complex/define.cpp
+++ icl-context/complex/define.cpp
@@ -1,14 +1,41 @@
 #include "define.h"
 
-namespace vm {
-namespace context {
-namespace complex {
+namespace vm::context::complex {
 
-Define::Define()
-{
+Define::Define () {
 
 }
 
-} // namespace complex
-} // namespace context
-} // namespace vm
+bool Define::checkPrev (const Context *context) const {
+	return context == nullptr;
+}
+
+bool Define::canBeAtEnd () const {
+	return false;
+}
+
+bool Define::isRightToLeft () const {
+	return true;
+}
+
+bool Define::isExecuable () const {
+	return true;
+}
+
+bool Define::execute () {
+
+}
+
+Context * Define::getBeginContext () {
+	return getFirst ();
+}
+
+Context * Define::getEndContext () {
+	return getLast ();
+}
+
+Context * Define::runMethod (const QString &name, memory::ParamList &params) {
+
+}
+
+} // namespace
--- icl-context/complex/define.h
+++ icl-context/complex/define.h
@@ -1,19 +1,29 @@
 #ifndef DEFINE_H
 #define DEFINE_H
 
+#include "complex.h"
 
-namespace vm {
-namespace context {
-namespace complex {
+
+
+namespace vm::context::complex {
 
 class Define : public Complex
 {
 public:
-	Define();
+	Define ();
+
+	// Context interface
+public:
+	bool checkPrev (const Context *context) const override;
+	bool canBeAtEnd () const override;
+	bool isRightToLeft () const override;
+	bool isExecuable () const override;
+	bool execute () override;
+	Context* getBeginContext () override;
+	Context* getEndContext () override;
+	Context* runMethod (const QString &name, memory::ParamList &params) override;
 };
 
-} // namespace complex
-} // namespace context
-} // namespace vm
+} // namespace
 
 #endif // DEFINE_H
