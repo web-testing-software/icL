0943eea53abf2d738be46b8359b2b13d2ae27eb4
Vasile Lelițac <lixcode@vivaldi.net>
2018-05-13 10:37:48 +0300

    Log.runMethos is READY.

--- icl-context/complex/log.cpp
+++ icl-context/complex/log.cpp
@@ -1,8 +1,44 @@
 #include "log.h"
 
+#include <object/object.h>
+
 namespace vm::context::complex {
 
 Log::Log() {}
+
+void Log::log(Log::Level level, const QString& message) {
+	int levelAsInt = 0;
+
+	if (level == Level::Warm) {
+		levelAsInt = 1;
+	}
+	else if (level == Level::Info) {
+		levelAsInt = 2;
+	}
+
+	emit newLog(levelAsInt, message);
+}
+
+void Log::runAnything(Log::Level level, memory::ArgList& args) {
+	if (args.length() == 1 && args[0].object->type() == memory::Type::String) {
+		log(level, args[0].object->getValue().toString());
+	}
+	else {
+		sendWrongArglist(args, "<String>");
+	}
+}
+
+void Log::runInfo(memory::ArgList& args) {
+	runAnything(Level::Info, args);
+}
+
+void Log::runWarm(memory::ArgList& args) {
+	runAnything(Level::Warm, args);
+}
+
+void Log::runError(memory::ArgList& args) {
+	runAnything(Level::Error, args);
+}
 
 bool Log::checkPrev(const Context* context) const {
 	return context == nullptr;
@@ -12,6 +48,21 @@ bool Log::canBeAtEnd() const {
 	return false;
 }
 
-Context* Log::runMethod(const QString& name, memory::ArgList &args) {}
+Context* Log::runMethod(const QString& name, memory::ArgList& args) {
+	if (name == "info") {
+		runInfo(args);
+	}
+	else if (name == "warm") {
+		runWarm(args);
+	}
+	else if (name == "error") {
+		runError(args);
+	}
+	else {
+		Context::runMethod(name, args);
+	}
+
+	return newContext;
+}
 
 }  // namespace vm::context::complex
--- icl-context/complex/log.h
+++ icl-context/complex/log.h
@@ -9,8 +9,22 @@ namespace vm::context::complex {
 
 class Log : Complex
 {
+	enum class Level {Info, Warm, Error};
+
 public:
 	Log();
+
+	void log(Level level, const QString& message);
+
+private:
+	void runAnything (Level level, memory::ArgList& args);
+
+	void runInfo (memory::ArgList& args);
+	void runWarm (memory::ArgList& args);
+	void runError (memory::ArgList& args);
+
+signals:
+	void newLog (int level, const QString& message);
 
 	// Context interface
 public:
