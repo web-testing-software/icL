d080f483a45e862d2cab0ff97f1143e8f8cf0365
Vasile Lelițac <lixcode@vivaldi.net>
2018-05-13 11:51:16 +0300

    Void cast function fixed.

--- icl-context/object/void.cpp
+++ icl-context/object/void.cpp
@@ -6,22 +6,27 @@ Void::Void()
 	: Object(QVariant(), true) {}
 
 bool Void::toBoolean() {
+	newValue = false;
 	return false;
 }
 
 int Void::toInt() {
+	newValue = 0;
 	return 0;
 }
 
 double Void::toDouble() {
+	newValue = 0.0;
 	return 0.0;
 }
 
 const QString Void::toString() {
+	newValue = QString{};
 	return QString{};
 }
 
 const QStringList Void::toList() {
+	newValue = QStringList{};
 	return QStringList{};
 };
 
