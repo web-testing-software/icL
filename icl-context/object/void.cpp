5e4f8ec9272db13caea6587f3f10e354ce7426b0
Vasile Lelițac <lixcode@vivaldi.net>
2018-05-13 11:47:57 +0300

    void cast function are READY.

--- icl-context/object/void.cpp
+++ icl-context/object/void.cpp
@@ -3,8 +3,26 @@
 namespace vm::context::object {
 
 Void::Void()
-	: Object(QVariant(), true){
+	: Object(QVariant(), true) {}
 
-	  };
+bool Void::toBoolean() {
+	return false;
+}
+
+int Void::toInt() {
+	return 0;
+}
+
+double Void::toDouble() {
+	return 0.0;
+}
+
+const QString Void::toString() {
+	return QString{};
+}
+
+const QStringList Void::toList() {
+	return QStringList{};
+};
 
 }  // namespace vm::context::object
