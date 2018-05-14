8516a52ac801e54dfd925bcd987050a806d6e2e5
Vasile Lelițac <lixcode@vivaldi.net>
2018-05-14 14:10:12 +0300

    Add headers for Element properties and methods.

--- icl-context/context-base/object/element.h
+++ icl-context/context-base/object/element.h
@@ -3,6 +3,8 @@
 
 #include "object.h"
 
+#include <icl-memory/structures/webelement.h>
+
 
 /**
  * vm
@@ -27,6 +29,76 @@ public:
 	Element(memory::DataState* container, const QString& varName);
 	Element(const QVariant& rvalue, bool readonly = false);
 	Element(const Object* const object);
+
+	// properties
+public:
+	// R/W properties will return a r/w object
+	// HTML -> String
+	// Text -> String
+	// Width -> Int
+	// Height -> Int
+
+	// Read only properties
+	bool visible();
+	bool clickable();
+
+private:
+	void runHTML();
+	void runText();
+	void runWidth();
+	void runHeight();
+
+	void runVisible();
+	void runClickable();
+
+	// methods
+public:
+	void scrollTo();
+	void click();
+	void sendKeys(const QString& keys);
+	void CtrlV(const QString& text);
+	bool isValid();
+
+	void add(memory::WebElement element);
+
+	memory::WebElement copy();
+	memory::WebElement filter(const QString& selector);
+	memory::WebElement filter(const QString& context, bool asfragment);
+	memory::WebElement get(int index);
+
+	memory::WebElement next();
+	memory::WebElement prev();
+	memory::WebElement parent();
+	memory::WebElement child(int index);
+	memory::WebElement closest(const QString& selector);
+
+	void addClass(const QString& className);
+	void removeClass(const QString& className);
+	bool hasClass(const QString& className);
+
+private:
+	void runScrollTo(memory::ArgList& args);
+	void runClick(memory::ArgList& args);
+	void runSendKeys(memory::ArgList& args);
+	void runCtrlV(memory::ArgList& args);
+	void runIsValid(memory::ArgList& args);
+
+	void runAdd(memory::ArgList& args);
+
+	void runCopy(memory::ArgList& args);
+	// both C++ filter functions are closed by this one
+	void runFilter(memory::ArgList& args);
+	void runGet(memory::ArgList& args);
+
+	void runNext(memory::ArgList& args);
+	void runPrev(memory::ArgList& args);
+	void runParent(memory::ArgList& args);
+	void runChild(memory::ArgList& args);
+	void runClosest(memory::ArgList& args);
+
+	void runAddClass(memory::ArgList& args);
+	void runRemoveClass(memory::ArgList& args);
+	void runHasClass(memory::ArgList& args);
 
 	// Context interface
 public:
