aee96b36c7012b93c83defc62d16b819ba7536e8
Vasile Lelițac <lixcode@vivaldi.net>
2018-06-06 09:26:16 +0300

    Add license text.

--- graduation/src2pdf.sh
+++ graduation/src2pdf.sh
@@ -1,9 +1,29 @@
 #!/bin/bash -e
 
-PYTHON_PATH=$(which python)
-return_status_1=$(echo $? 2> /dev/null)
-PIP_PATH=$(which pip 2> /dev/null)
-return_status_2=$(echo $?)
+# Forked form https://github.com/arosspope/src2pdf.git
+# Source License: MIT
+
+# MIT License
+#
+# Copyright (c) 2017 Andrew Pope
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
 tex_file="Исходники"
 
@@ -13,7 +33,7 @@ file_types[1]="cpp|-name '*.h' -o -name '*.cpp'"
 file_types[2]="qml|-name '*.qml'"
 file_types[3]="js|-name '*.js'"
 file_types[4]="sql|-name '*.sql'"
-# DONE EDITING! :)
+
 
 input_minted_header () {
   tex_file=$1
