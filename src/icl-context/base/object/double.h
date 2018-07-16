65764f5e467ceec5b4c8e1f5e9d9937433f9b22c
Vasile Lelițac <lixcode@vivaldi.net>
2018-07-16 10:52:05 +0300

    context.object.Double was documented.

--- src/icl-context/base/object/double.h
+++ src/icl-context/base/object/double.h
@@ -4,30 +4,44 @@
 #include "object.h"
 
 
-/**
- *  icL
- *  |- app
- *  |- context
- *  |  |- complex
- *  |  |- code
- *  |  |  '- control
- *  |  |     '- catch0
- *  |  |- data
- *  |  '-> object
- *  |- inter
- *  |- logic
- *  |  |- cross
- *  |  '- rich
- *  '- memory
- */
 namespace icL::context::object {
 
 class Double : public Object
 {
 public:
+	/**
+	 * \~english
+	 * @brief Double calls the contructor of class Object
+	 * @param il is a base contructor argument
+	 * @param container is a base contructor argument
+	 * @param varName is a base contructor argument
+	 */
 	Double(memory::InterLevel * il, memory::DataState * container, const QString & varName);
+
+	/**
+	 * \~english
+	 * @brief Double calls the contructor of class Object
+	 * @param il is a base contructor argument
+	 * @param rvalue is a base contructor argument
+	 * @param readonly is a base contructor argument
+	 */
 	Double(memory::InterLevel * il, const QVariant & rvalue, bool readonly = false);
+
+	/**
+	 * \~english
+	 * @brief Double calls the contructor of class Object
+	 * @param il is a base contructor argument
+	 * @param getter is a base contructor argument
+	 * @param setter is a base contructor argument
+	 */
 	Double(memory::InterLevel * il, const QString & getter, const QString & setter);
+
+	/**
+	 * \~english
+	 * @brief Double calls the contructor of class Object
+	 * @param il is a base contructor argument
+	 * @param object is a base contructor argument
+	 */
 	Double(memory::InterLevel * il, const Object * const object);
 
 	// Context interface
@@ -36,6 +50,9 @@ public:
 
 	// Object interface
 public:
+	/**
+	 * \~english @return memory.Type.Double
+	 */
 	memory::Type type() const override;
 
 	bool              toBoolean() override;
