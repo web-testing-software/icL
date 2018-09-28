e14284d3e3573efccd83ef4bc27be449ef0d4e22
Vasile Lelițac <lixcode@vivaldi.net>
2018-09-28 11:31:29 +0300

    Add look.Link class

---
+++ src/icL-look/base/link.cpp
@@ -0,0 +1,56 @@
+#include "link.h"
+
+namespace icL::look::base {
+
+Link::Link(QObject * parent)
+	: QObject(parent) {}
+
+Link::~Link() {
+	if (m_normal != nullptr)
+		delete m_normal;
+	if (m_hover != nullptr)
+		delete m_hover;
+	if (m_active != nullptr)
+		delete m_active;
+}
+
+Text * Link::normal() const {
+	return m_normal;
+}
+
+Text * Link::hover() const {
+	return m_hover;
+}
+
+Text * Link::active() const {
+	return m_active;
+}
+
+void Link::setNormal(Text * normal) {
+	if (m_normal == normal) {
+		return;
+	}
+
+	m_normal = normal;
+	emit normalChanged(m_normal);
+}
+
+void Link::setHover(Text * hover) {
+	if (m_hover == hover) {
+		return;
+	}
+
+	m_hover = hover;
+	emit hoverChanged(m_hover);
+}
+
+void Link::setActive(Text * active) {
+	if (m_active == active) {
+		return;
+	}
+
+	m_active = active;
+	emit activeChanged(m_active);
+}
+
+}  // namespace icL::look::base
---
+++ src/icL-look/base/link.h
@@ -0,0 +1,79 @@
+#ifndef icL_look_base_Link
+#define icL_look_base_Link
+
+#include "text.h"
+
+#include <QObject>
+
+namespace icL::look::base {
+
+/**
+ * @brief The Link class describes a look for links
+ */
+class Link : public QObject
+{
+	Q_OBJECT
+
+	Q_PROPERTY(Text * normal READ normal WRITE setNormal NOTIFY normalChanged)
+	Q_PROPERTY(Text * hover READ hover WRITE setHover NOTIFY hoverChanged)
+	Q_PROPERTY(Text * active READ active WRITE setActive NOTIFY activeChanged)
+
+	Text * m_normal = nullptr;
+	Text * m_hover  = nullptr;
+	Text * m_active = nullptr;
+
+public:
+	/**
+	 * @brief Link is the default constructor
+	 * @param parent is the default QObject arg
+	 */
+	explicit Link(QObject * parent = nullptr);
+
+	~Link();
+
+	/**
+	 * @brief normal is the default text color
+	 * @return the default text color
+	 */
+	Text * normal() const;
+
+	/**
+	 * @brief hover is the hovered text color
+	 * @return the hovered text color
+	 */
+	Text * hover() const;
+
+	/**
+	 * @brief active is the active link color
+	 * @return the active link color
+	 */
+	Text * active() const;
+
+signals:
+	void normalChanged(Text * normal);
+	void hoverChanged(Text * hover);
+	void activeChanged(Text * active);
+
+public slots:
+	/**
+	 * @brief setNormal changes the default text color
+	 * @param normal is the new default text color
+	 */
+	void setNormal(Text * normal);
+
+	/**
+	 * @brief setHover changes the hovered text color
+	 * @param hover is the new hovered text color
+	 */
+	void setHover(Text * hover);
+
+	/**
+	 * @brief setActive changes the active text color
+	 * @param active is the new active text color
+	 */
+	void setActive(Text * active);
+};
+
+}  // namespace icL::look::base
+
+#endif  // icL_look_base_Link
