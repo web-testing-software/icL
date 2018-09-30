e9f66e00fcf3ae69b647b0a0bd6378f21896af85
Vasile Lelițac <lixcode@vivaldi.net>
2018-09-30 12:28:03 +0300

    Add CharFormatBase class in look.Line to minimise members number.

--- src/icL-look/editor/line.cpp
+++ src/icL-look/editor/line.cpp
@@ -9,11 +9,8 @@ QColor Line::lineBg() const {
 	return m_lineBg;
 }
 
-QColor Line::lineNumberBg() const {
-	return m_lineNumberBg;
-}
-
-QColor Line::lineNumber() const {
+CharFormatBase *Line::lineNumber() const
+{
 	return m_lineNumber;
 }
 
@@ -25,15 +22,8 @@ void Line::setLineBg(QColor lineBg) {
 	emit lineBgChanged(m_lineBg);
 }
 
-void Line::setLineNumberBg(QColor lineNumberBg) {
-	if (m_lineNumberBg == lineNumberBg)
-		return;
-
-	m_lineNumberBg = lineNumberBg;
-	emit lineNumberBgChanged(m_lineNumberBg);
-}
-
-void Line::setLineNumber(QColor lineNumber) {
+void Line::setLineNumber(CharFormatBase *lineNumber)
+{
 	if (m_lineNumber == lineNumber)
 		return;
 
--- src/icL-look/editor/line.h
+++ src/icL-look/editor/line.h
@@ -1,6 +1,8 @@
 #ifndef icL_look_editor_Line
 #define icL_look_editor_Line
 
+#include "charformatbase.h"
+
 #include <QColor>
 #include <QObject>
 
@@ -14,14 +16,12 @@ class Line : public QObject
 	Q_OBJECT
 
 	// clang-format off
-	Q_PROPERTY(QColor       lineBg READ lineBg       WRITE setLineBg       NOTIFY lineBgChanged)
-	Q_PROPERTY(QColor lineNumberBg READ lineNumberBg WRITE setLineNumberBg NOTIFY lineNumberBgChanged)
-	Q_PROPERTY(QColor   lineNumber READ lineNumber   WRITE setLineNumber   NOTIFY lineNumberChanged)
+	Q_PROPERTY(QColor              lineBg READ lineBg     WRITE setLineBg     NOTIFY lineBgChanged)
+	Q_PROPERTY(CharFormatBase* lineNumber READ lineNumber WRITE setLineNumber NOTIFY lineNumberChanged)
 	// clang-format on
 
 	QColor m_lineBg;
-	QColor m_lineNumberBg;
-	QColor m_lineNumber;
+	CharFormatBase* m_lineNumber;
 
 public:
 	/**
@@ -37,41 +37,27 @@ public:
 	QColor lineBg() const;
 
 	/**
-	 * @brief lineNumberBg is the backgound color of line number of highlighted
-	 * line
-	 * @return the background color for line number of highlighted line
+	 * @brief lineNumber is the look of line number
+	 * @return the look for line number
 	 */
-	QColor lineNumberBg() const;
-
-	/**
-	 * @brief lineNumber is the color of line number
-	 * @return the color for line number
-	 */
-	QColor lineNumber() const;
+	CharFormatBase* lineNumber() const;
 
 signals:
 	void lineBgChanged(QColor lineBg);
-	void lineNumberBgChanged(QColor lineNumberBg);
-	void lineNumberChanged(QColor lineNumber);
+	void lineNumberChanged(CharFormatBase* lineNumber);
 
 public slots:
 	/**
 	 * @brief setLineBg changes the background of the highlighted line
 	 * @param lineBg is the new background color for highlighted line
 	 */
-	void setLineBg(QColor lineBg);
+void setLineBg(QColor lineBg);
 
-	/**
-	 * @brief setLineNumberBg changes the background color of line number
-	 * @param lineNumberBg is the new backgound color for line number
-	 */
-	void setLineNumberBg(QColor lineNumberBg);
-
-	/**
-	 * @brief setLineNumber changes the color of line number
-	 * @param lineNumber is the new color for line number
-	 */
-	void setLineNumber(QColor lineNumber);
+/**
+ * @brief setLineNumber changes the look of line numbers
+ * @param lineNumber is the new look for line number
+ */
+void setLineNumber(CharFormatBase* lineNumber);
 };
 
 }  // namespace icL::look::editor
