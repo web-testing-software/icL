86b49c1254f844ff4a5371bfd1262a2a05cabe4f
Vasile Lelițac <lixcode@vivaldi.net>
2018-10-12 14:46:54 +0300

    Added toolkit.Script.setPath method.

--- src/icL-toolkit/session/script.cpp
+++ src/icL-toolkit/session/script.cpp
@@ -1,11 +1,27 @@
 #include "script.h"
 
+#include <QFile>
 #include <QVariant>
 
 namespace icL::toolkit::session {
 
 Script::Script(Item * parent)
 	: Finaly(parent) {}
+
+bool Script::setPath(const QString & path) {
+	QFile file{path};
+
+	if (!file.open(QFile::ReadOnly)) {
+		return false;
+	}
+
+	file.close();
+
+	int slash = path.lastIndexOf('/');
+	name      = path.mid(slash + 1);
+
+	return true;
+}
 
 int Script::columnCount() {
 	return 1;
