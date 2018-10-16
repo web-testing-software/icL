fe8076a56bc133b5a70a31da059e7e41b64fa227
Vasile Lelițac <lixcode@vivaldi.net>
2018-10-16 20:58:52 +0300

    editor.Fragment class is ready.

--- src/icL-editor/private/fragment.cpp
+++ src/icL-editor/private/fragment.cpp
@@ -2,9 +2,55 @@
 
 namespace icL::editor {
 
-Fragment::Fragment(QObject *parent) : QObject(parent)
-{
+Fragment::Fragment(QObject * parent)
+	: QObject(parent) {}
 
+Fragment * Fragment::prev() const {
+	return m_prev;
 }
 
-} // namespace
+Fragment * Fragment::next() const {
+	return m_next;
+}
+
+Line * Fragment::line() const {
+	return m_line;
+}
+
+uint8_t Fragment::length() const {
+	return m_length;
+}
+
+int8_t Fragment::spaces() const {
+	return m_spaces;
+}
+
+int8_t Fragment::glyphs() const {
+	return m_glyphs;
+}
+
+void Fragment::setPrev(Fragment * prev) {
+	if (m_prev == prev)
+		return;
+
+	m_prev = prev;
+	emit prevChanged(m_prev);
+}
+
+void Fragment::setNext(Fragment * next) {
+	if (m_next == next)
+		return;
+
+	m_next = next;
+	emit nextChanged(m_next);
+}
+
+void Fragment::setLine(Line * line) {
+	if (m_line == line)
+		return;
+
+	m_line = line;
+	emit lineChanged(m_line);
+}
+
+}  // namespace icL::editor
--- src/icL-editor/private/fragment.h
+++ src/icL-editor/private/fragment.h
@@ -9,6 +9,8 @@ class QStaticText;
 
 namespace icL::editor {
 
+class Line;
+
 class Fragment : public QObject
 {
 	Q_OBJECT
@@ -16,15 +18,51 @@ class Fragment : public QObject
 	// clang-format off
 	Q_PROPERTY(icL::editor::Fragment* prev READ prev WRITE setPrev NOTIFY prevChanged)
 	Q_PROPERTY(icL::editor::Fragment* next READ next WRITE setNext NOTIFY nextChanged)
-	Q_PROPERTY(icL::Editor::Line*     line READ line WRITE setLine NOTIFY lineChanged)
+	Q_PROPERTY(icL::editor::Line*     line READ line WRITE setLine NOTIFY lineChanged)
 
-	Q_PROPERTY(uint8_t length READ length WRITE setLength NOTIFY lengthChanged)
-	Q_PROPERTY(int8_t  spaces READ spaces WRITE setSpaces NOTIFY spacesChanged)
-	Q_PROPERTY(int8_t  glyphs READ glyphs WRITE setGlyphs NOTIFY glyphsChanged)
+	Q_PROPERTY(uint8_t length READ length NOTIFY lengthChanged)
+	Q_PROPERTY(int8_t  spaces READ spaces NOTIFY spacesChanged)
+	Q_PROPERTY(int8_t  glyphs READ glyphs NOTIFY glyphsChanged)
 	// clang-format on
 
 public:
 	explicit Fragment(QObject * parent = nullptr);
+
+	/**
+	 * @brief prev is the previous sibling
+	 * @return the previous sibling
+	 */
+	Fragment * prev() const;
+
+	/**
+	 * @brief next is the next sibling
+	 * @return the next sibling
+	 */
+	Fragment * next() const;
+
+	/**
+	 * @brief line is the line which contains this fragment
+	 * @return the line of fragment
+	 */
+	Line * line() const;
+
+	/**
+	 * @brief length is the length of fragment (spaces + glyphs)
+	 * @return the length of fragment
+	 */
+	uint8_t length() const;
+
+	/**
+	 * @brief spaces is the number of spaces before glyphs
+	 * @return the number of spaces before glyphs
+	 */
+	int8_t spaces() const;
+
+	/**
+	 * @brief glyphs is the number of glyphs
+	 * @return the number of glyphs
+	 */
+	int8_t glyphs() const;
 
 	/**
 	 * @brief displayText gets the text to display
@@ -89,9 +127,44 @@ public:
 	virtual void replace(int p1, int p2, const QString & after);
 
 signals:
+	void prevChanged(Fragment * prev);
+	void nextChanged(Fragment * next);
+	void lineChanged(Line * line);
+	void lengthChanged(uint8_t length);
+	void spacesChanged(int8_t spaces);
+	void glyphsChanged(int8_t glyphs);
 
 public slots:
+	/**
+	 * @brief setPrev changes the previous sibling fragment
+	 * @param prev is the new previous sibling fragment
+	 */
+	void setPrev(Fragment * prev);
+
+	/**
+	 * @brief setNext changes the next sibling fragment
+	 * @param next is the new next sibling fragment
+	 */
+	void setNext(Fragment * next);
+
+	/**
+	 * @brief setLine chages the parent of fragment
+	 * @param line is the new parent of fragment
+	 */
+	void setLine(Line * line);
+
+private:
+	// Properties
+	Fragment * m_prev;
+	Fragment * m_next;
+	Line *     m_line;
+
+	uint8_t m_length;
+	int8_t  m_spaces;
+	int8_t  m_glyphs;
 };
+
+
 
 }  // namespace icL::editor
 
