09d975bb7f6b9fa2bc67b627b44e2f9c0f808a42
Vasile Lelițac <lixcode@vivaldi.net>
2018-10-16 21:27:24 +0300

    editor.Selection is ready.

--- src/icL-editor/private/selection.cpp
+++ src/icL-editor/private/selection.cpp
@@ -2,9 +2,27 @@
 
 namespace icL::editor {
 
-Selection::Selection(QObject *parent) : QObject(parent)
-{
+Selection::Selection(QObject * parent)
+	: QObject(parent) {}
 
+Cursor * Selection::begin() const {
+	return m_begin;
 }
 
-} // namespace
+Cursor * Selection::end() const {
+	return m_end;
+}
+
+Selection * Selection::next() const {
+	return m_next;
+}
+
+void Selection::setNext(Selection * next) {
+	if (m_next == next)
+		return;
+
+	m_next = next;
+	emit nextChanged(m_next);
+}
+
+}  // namespace icL::editor
--- src/icL-editor/private/selection.h
+++ src/icL-editor/private/selection.h
@@ -5,20 +5,44 @@
 
 namespace icL::editor {
 
+class Cursor;
+
 class Selection : public QObject
 {
 	Q_OBJECT
 
 	// clang-format off
-	Q_PROPERTY(icL::editor::Cursor*   begin READ begin WRITE setBegin NOTIFY beginChanged)
-	Q_PROPERTY(icL::editor::Cursor*     end READ end   WRITE setEnd   NOTIFY endChanged)
+	Q_PROPERTY(icL::editor::Cursor*   begin READ begin NOTIFY beginChanged)
+	Q_PROPERTY(icL::editor::Cursor*     end READ end   NOTIFY endChanged)
 	Q_PROPERTY(icL::editor::Selection* next READ next  WRITE setNext  NOTIFY nextChanged)
 
 	Q_PROPERTY(bool rtl READ rtl WRITE setRtl NOTIFY rtlChanged)
 	// clang-format on
 
+	Cursor *    m_begin;
+	Cursor *    m_end;
+	Selection * m_next;
+
 public:
-	explicit Selection(QObject *parent = nullptr);
+	explicit Selection(QObject * parent = nullptr);
+
+	/**
+	 * @brief begin is the begin of selection
+	 * @return the begin of selection
+	 */
+	Cursor * begin() const;
+
+	/**
+	 * @brief end is the end of selection
+	 * @return the end of selection
+	 */
+	Cursor * end() const;
+
+	/**
+	 * @brief next is next linked selection
+	 * @return the next linked selection
+	 */
+	Selection * next() const;
 
 	/**
 	 * @brief move moves the cursor to left/right be needed chars
@@ -42,10 +66,18 @@ public:
 	void moveUpDown(int lines, bool select = false);
 
 signals:
+	void beginChanged(Cursor * begin);
+	void endChanged(Cursor * end);
+	void nextChanged(Selection * next);
 
 public slots:
+	/**
+	 * @brief setNext add a new selection to collection
+	 * @param next is the new selection in collection
+	 */
+	void setNext(Selection * next);
 };
 
-} // namespace
+}  // namespace icL::editor
 
-#endif // icL_editor_Selection
+#endif  // icL_editor_Selection
